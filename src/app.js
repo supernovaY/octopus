8c72fd002e2c14a34d4665395da42eba1d528520
Supernova <supernova_gu@outlook.com>
2021-03-09 17:32:41 +0800

    完成了 row and col组件

--- src/app.js
+++ src/app.js
@@ -3,11 +3,15 @@ import Button from './button'
 import Icon from './icon'
 import OctopusButtonGroup from './button-group'
 import Input from './input'
+import Col from './col'
+import Row from './row'
 
 Vue.component('g-button', Button)
 Vue.component('g-icon', Icon)
 Vue.component('g-button-group', OctopusButtonGroup)
 Vue.component('g-input', Input)
+Vue.component('g-row', Row)
+Vue.component('g-col', Col)
 
 new Vue({
     el: '#app',
